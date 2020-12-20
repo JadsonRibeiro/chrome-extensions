5a3baff421e1dde374ca13a93f50e458d15b5077
Jadson Ribeiro (Notebook) <jdsntj@gmail.com>
2020-12-19 23:11:40 -0300

    Speed Changer extension concluded

---
+++ speed-changer/README.md
@@ -0,0 +1,3 @@
+Extensão para acelerar em 2x a velocidade dos áudios do Whatsapp.
+
+A extensão adicionará um botão ao cabeçalho do Whatsapp Web que, ao clicar, acelerará em 2x todos os áudios.
---
+++ speed-changer/content.js
@@ -0,0 +1,20 @@
+const interval = setInterval(() => {
+    const header  = document.querySelector('.chGSa');
+    if(header) {
+        console.log('Header', header);
+        clearInterval(interval);
+
+        const button = document.createElement("button");
+        button.innerHTML = "2x";
+        button.classList.add("twoTimesButton");
+
+        button.addEventListener("click", () => {
+            const audios = document.querySelectorAll("audio");
+            audios.forEach(audio => {
+                audio.playbackRate = 2;
+            });
+        });
+
+        header.appendChild(button);
+    }
+}, 1000);
